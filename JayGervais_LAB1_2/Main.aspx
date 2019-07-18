f5b3c6a68817d7e98fb82e632109b872d2020098
Jay Gervais <mrjaygervais@gmail.com>
2019-07-17 22:48:24 -0600

    Add project files.

---
+++ JayGervais_LAB1_2/Main.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="JayGervais_LAB1_2.Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Temperature Converter</title>
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <link href="Styles.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="page-header jumbotron">
+    <h1 class="headertxt">Temperature Converter</h1>
+  </div>
+    <div class="container">
+        <form id="form1" runat="server">
+        <div>
+
+       <div class="form-group">
+        <div class="row">
+  		    <div class ="col-11">
+               <div class="container">
+               <div class="form-container">            
+                   <div class="row">
+                    <div class ="col-lg-4">
+                    <asp:Label ID="lblFrom" runat="server" Text="From"></asp:Label>
+                    <asp:DropDownList class="form-control" ID="ddlTempSystemsFrom" runat="server" AppendDataBoundItems="True" AutoPostBack="True">
+                        </asp:DropDownList>
+                    </div>
+                    <div class ="col-lg-4">
+                    <asp:Label ID="lblTo" runat="server" Text="To"></asp:Label>
+                    <asp:DropDownList class="form-control" ID="ddlTempSystemsTo" runat="server" AppendDataBoundItems="True">
+                        </asp:DropDownList>
+                    </div>
+                </div>
+                <div class="pt-3">
+                    <asp:Label ID="lblInputTemp" runat="server" Text="Input Temperature"></asp:Label>&nbsp;
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidatorTemp" runat="server" ControlToValidate="txtTempInput" Display="Dynamic" ErrorMessage="Input temperature is required" ForeColor="#990000"></asp:RequiredFieldValidator>
+                    <asp:RangeValidator ID="RangeValidatorInputTemp" runat="server" ControlToValidate="txtTempInput" Display="Dynamic" ErrorMessage="Add a number between -1000 to 1000" ForeColor="#9900CC" MaximumValue="1000" MinimumValue="-1000" Type="Integer"></asp:RangeValidator>
+                    <asp:TextBox class="form-control" ID="txtTempInput" runat="server" MaxLength="50"></asp:TextBox>
+                </div>
+                <div class="pt-3" aria-readonly="True">
+                    <asp:Label ID="lblConversion" runat="server" Text="Result"></asp:Label>
+                    <asp:TextBox class="form-control" ID="txtCalcResult" runat="server" MaxLength="50" ReadOnly="True"></asp:TextBox>
+                </div>
+                <div class="pt-4">
+                    <asp:Button ID="btnConvert" CssClass="btn btn-primary" runat="server" Text="Convert" OnClick="btnConvert_Click1" />&nbsp;<asp:Button ID="btnClear" CssClass="btn btn-primary" runat="server" Text="Clear" OnClick="btnClear_Click" CausesValidation="False" />
+                </div>
+                 </div>
+               </div>
+            
+          </div>
+            <div class ="col-1">
+                <div class="tempborder">
+                    <img class="thermpic" src="images/thermometer-1134182_1280.png" />
+                </div>   
+            </div>
+        </div>
+      </div>
+
+
+        </div>
+    </form>
+    </div>
+    
+</body>
+</html>
---
+++ JayGervais_LAB1_2/Main.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JayGervais_LAB1_2
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                tempDropDown(ddlTempSystemsTo);
+                tempDropDown(ddlTempSystemsFrom);
+                ddlTempSystemsTo.SelectedValue = "Fahrenheit";
+                ddlTempSystemsFrom.SelectedValue = "Celsius";
+            }
+        }
+
+        private void tempDropDown(DropDownList inputList)
+        {
+            List<ListItem> tempList = new List<ListItem>();
+            tempList.Add(new ListItem("Celsius", "Celsius"));
+            tempList.Add(new ListItem("Fahrenheit", "Fahrenheit"));
+            tempList.Add(new ListItem("Kelvin", "Kelvin"));
+            inputList.Items.AddRange(tempList.ToArray());
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCalcResult.Text = "";
+            txtTempInput.Text = "";
+            ddlTempSystemsFrom.SelectedValue = "Celsius";
+            ddlTempSystemsTo.SelectedValue = "Fahrenheit";
+        }
+
+        protected void btnConvert_Click1(object sender, EventArgs e)
+        {
+            if(txtTempInput.Text != "")
+            {
+                double temp = Convert.ToDouble(txtTempInput.Text);
+
+                if (ddlTempSystemsFrom.SelectedValue == "Celsius" && ddlTempSystemsTo.SelectedValue == "Celsius")
+                {
+                    txtCalcResult.Text = DoFormat(temp);
+                }
+                else if (ddlTempSystemsFrom.SelectedValue == "Celsius" && ddlTempSystemsTo.SelectedValue == "Fahrenheit")
+                {
+                    double CtoF = (temp * 9) / 5 + 32;
+                    txtCalcResult.Text = DoFormat(CtoF);
+                }
+                else if (ddlTempSystemsFrom.SelectedValue == "Celsius" && ddlTempSystemsTo.SelectedValue == "Kelvin")
+                {
+                    double CtoK = temp + 273.15;
+                    txtCalcResult.Text = DoFormat(CtoK);
+                }
+                else if (ddlTempSystemsFrom.SelectedValue == "Fahrenheit" && ddlTempSystemsTo.SelectedValue == "Celsius")
+                {
+                    double FtoC = (temp - 32) * 5.0 / 9.0;
+                    txtCalcResult.Text = DoFormat(FtoC);
+                }
+                else if (ddlTempSystemsFrom.SelectedValue == "Fahrenheit" && ddlTempSystemsTo.SelectedValue == "Fahrenheit")
+                {
+                    txtCalcResult.Text = DoFormat(temp);
+                }
+                else if (ddlTempSystemsFrom.SelectedValue == "Fahrenheit" && ddlTempSystemsTo.SelectedValue == "Kelvin")
+                {
+                    double FtoK = (temp - 32) * 5.0 / 9.0 + 273.15;
+                    txtCalcResult.Text = DoFormat(FtoK);
+                }
+                else if (ddlTempSystemsFrom.SelectedValue == "Kelvin" && ddlTempSystemsTo.SelectedValue == "Celsius")
+                {
+                    double KtoC = temp - 273.15;
+                    txtCalcResult.Text = DoFormat(KtoC);
+                }
+                else if (ddlTempSystemsFrom.SelectedValue == "Kelvin" && ddlTempSystemsTo.SelectedValue == "Fahrenheit")
+                {
+                    double KtoF = (((temp - 273.15) * 9) / 5) + 32;
+                    txtCalcResult.Text = DoFormat(KtoF);
+                }
+                else if (ddlTempSystemsFrom.SelectedValue == "Kelvin" && ddlTempSystemsTo.SelectedValue == "Kelvin")
+                {
+                    txtCalcResult.Text = DoFormat(temp);
+                }
+                else
+                {
+                    txtCalcResult.Text = "Did not work";
+                }
+            }
+        }
+
+        public static string DoFormat(double myNumber)
+        {
+            var s = string.Format("{0:0.00}", myNumber);
+
+            if (s.EndsWith("00"))
+            {
+                return ((int)myNumber).ToString();
+            }
+            else
+            {
+                return s;
+            }
+        }
+
+    }
+}
